d59934231494c0403274c4ea95b61f336500e9df
Meghna <2021mt93670@wilp.bits-pilani.ac.in>
2023-11-03 14:18:54 +0530

    add toggle update

--- src/components/Widget/components/HomePage/components/Header/Settings/index.tsx
+++ src/components/Widget/components/HomePage/components/Header/Settings/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactSwitch from 'react-switch';
-
+import axios from 'axios';
 import './style.scss';
 
 const Settings = ({ onClose, userSettings }) => {
@@ -8,15 +8,44 @@ const Settings = ({ onClose, userSettings }) => {
   const [checked, setChecked] = useState(true);
   const languagesList = ["en","es","en-us","en-uk"];
   const handleChange = val => {
+    console.log(val);
     setChecked(val)
+    updateChange(val)
   }
+  function updateChange(isChecked) {
+    const postData = {
+      username: "Anandh",
+      password: "test@12345"
+    }
+    axios.post('http://127.0.0.1:8000/user/auth/token/', postData).then(
+      response => {
+        console.log(response);
+        const authToken = response.data?.data.access;
+        const updateToggle = {
+          "use_chat_history": isChecked
+        }
+        axios.put('http://127.0.0.1:8000/user/update/',updateToggle, {
+          headers: {
+            'Authorization': `Bearer ${authToken}`
+          }
+        }).then(response => {
+          console.log(response);
+        })
+      }
+    )
+  }
+
+  useEffect(() => {
+    setChecked(userSettings.isChatHistoryOn);
+  },[])
+
   console.log(userSettings);
   return (
     <div className="settings">
       <div>
         Enable history :
       <ReactSwitch
-        checked={userSettings.isChatHistoryOn}
+        checked={checked}
         onChange={handleChange}
       />
       </div>
